94ba41e376ff6ba367790686ad989540aa15c019
rsp <siripokarpirom@gmail.com>
2017-07-01 17:17:30 +0700

    Add files via upload

---
+++ esp8266_rotary_encoder_pwm_demo-2/esp8266_rotary_encoder_pwm_demo-2.ino
@@ -0,0 +1,100 @@
+//////////////////////////////////////////////////////////////////////////
+// Author: RSP @ Embedded Systems Lab (ESL), KMUTNB, Bangkok / Thailand
+// Date: 2017-07-01
+// Arduino IDE: v1.8.2 + esp8266 v2.3.0
+// Boards with ESP-12E
+//////////////////////////////////////////////////////////////////////////
+
+#include <string.h>
+// for Windows 
+#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+// for Linux
+//#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#include <ESP8266WiFi.h>
+
+#define VALUE_MAX 255   // max. value (range: 0..VALUE_MAX) for PWM
+
+// constants
+const int QA_PIN = 14;      // D5 = GPIO14 (QA or CLK pin of the rotary encoder switch)
+const int QB_PIN = 12;      // D6 = GPIO12 (QB or DT  pin of the rotary encoder switch)
+const int PB_PIN = 2;       // D4 = GPIO2  (SW pin of the rotary encoder switch, pusb button, active-low)
+const int PWM_LED_PIN = 4;  // D2 = GPIO4
+
+// global variables
+char sbuf[64];
+volatile int last_a = 1, last_b = 1;
+volatile int16_t encoder_cnt = 0;
+volatile bool encoder_update = false;
+volatile bool button_pressed = false;
+
+// forward declarations 
+void encode_inputs();
+void button_press_action();
+
+void button_press_action(){
+  button_pressed = true;
+}
+
+void encode_inputs() {
+  detachInterrupt( QA_PIN );
+  int a = digitalRead(QA_PIN);
+  int b = digitalRead(QB_PIN);
+  if (( a != last_a ) || ( b != last_b ) ) {
+     if ( a == b ) {
+       encoder_cnt++;
+    } else { 
+       encoder_cnt--;
+    }
+    last_a = a;
+    last_b = b;
+    encoder_update = true;
+    if ( encoder_cnt < 0 ) { encoder_cnt = 0; }
+    if ( encoder_cnt > VALUE_MAX ) { encoder_cnt = VALUE_MAX; }
+  }
+  attachInterrupt( QA_PIN, encode_inputs, CHANGE );
+}
+
+void setup() {
+  Serial.begin( 115200 );
+  Serial.println( F("\n\n\n") );
+  delay(100);
+  
+  pinMode( QA_PIN, INPUT );
+  pinMode( QB_PIN, INPUT );
+  pinMode( PB_PIN, INPUT_PULLUP );
+  attachInterrupt( QA_PIN, encode_inputs, CHANGE );
+  attachInterrupt( PB_PIN, button_press_action, FALLING );
+  pinMode( PWM_LED_PIN, OUTPUT );
+  
+  Serial.println( F("ESP8266 Rotary Encoder Demo") );
+  sprintf_P( sbuf, PSTR("Sketch: '%s'"), __FILENAME__ );
+  Serial.println( sbuf );
+  Serial.println( "Build: " __DATE__ ", " __TIME__ );
+
+  WiFi.disconnect();
+  WiFi.mode(WIFI_OFF);  // disable WiFi
+
+  analogWriteFreq( 500 ); // set PWM frequency: 500Hz 
+  analogWriteRange(VALUE_MAX+1); // set PWM duty cycle range 
+}
+
+void loop() {
+  if ( button_pressed ) {
+     button_pressed = false;
+     encoder_cnt = 0;
+     Serial.println( F("Reset encoder counter") );
+     analogWrite( PWM_LED_PIN, encoder_cnt );
+  }
+  if ( encoder_update ) {
+     encoder_update = false;
+     if ( encoder_cnt >= 0 && encoder_cnt <= 255 ) {
+        analogWrite( PWM_LED_PIN, encoder_cnt );
+        sprintf_P( sbuf, PSTR("PWM duty cycle: %d"), encoder_cnt );
+        Serial.println( sbuf );
+     }
+  }
+  delay(1);
+}
+//////////////////////////////////////////////////////////////////////////
+
