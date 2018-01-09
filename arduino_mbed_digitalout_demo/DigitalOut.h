d2d05da2a44b650f7ce9380b92f24e211413f123
rsp <siripokarpirom@gmail.com>
2018-01-09 09:44:14 +0700

    Add files via upload

---
+++ arduino_mbed_digitalout_demo/DigitalOut.h
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////
+#ifndef __DIGITAL_OUT_H
+#define __DIGITAL_OUT_H
+///////////////////////////////////////////////////////////////////////
+
+class DigitalOut {
+public:
+  DigitalOut( int pin, int value=0 ) : _pin(pin), _value(value) {
+     pinMode( _pin, OUTPUT );
+     digitalWrite( _pin, _value );
+  }
+  
+  inline void write( int value ) {
+     digitalWrite( _pin, _value = value );
+  }
+  
+  inline int read() { 
+     return _value; 
+  }
+  
+  inline operator int() { 
+     return this->read(); 
+  }
+  
+  inline DigitalOut& operator=( DigitalOut& rhs ) {
+     this->write( rhs.read() );
+     return *this;
+  }
+  
+  inline DigitalOut& operator=( int value ) {
+     this->write( value );
+     return *this;
+  }
+
+private:
+  int _pin, _value;
+
+};
+
+#endif // __DIGITAL_OUT_H
+///////////////////////////////////////////////////////////////////////
+
+
---
+++ arduino_mbed_digitalout_demo/arduino_mbed_digitalout_demo.ino
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// Author: Rawat S. (KMUTNB, Bangkok / Thailand
+// Date: 2018-01-09
+//////////////////////////////////////////////////////////////////////////////////////////
+
+#include "Arduino.h"
+#include "DigitalOut.h"
+
+#define wait_ms(dly)   delay(dly)
+
+DigitalOut redLed( D5 );
+DigitalOut blueLed( D6 );
+
+void setup() {
+    redLed  = 0;  // turn off the Red LED
+    blueLed = 1;  // turn on the Blue LED
+    wait_ms( 100 );
+    Serial.begin( 115200 );
+    Serial.printf( "Toggle two LEDs\n" );
+    Serial.printf( "Initial states: Red=%d, Blue=%d\n", redLed.read(), blueLed.read() );
+}
+
+void loop() {
+    redLed  = !redLed;  // toggle the Red LED pin
+    blueLed = !blueLed; // toggle the Blue LED pin
+    wait_ms( 100 ); // delay in msec
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
