1d72d5b620e401f85e52f8a3f1173d80b0439dd2
rsp <siripokarpirom@gmail.com>
2018-04-20 15:52:11 +0700

    Add files via upload

---
+++ esp8266_pcf8574a_lcd_si7021_demo/SI7021.h
@@ -0,0 +1,120 @@
+////////////////////////////////////////////////////////////////////////////////
+// Author: RSP
+// IoT Engineering Education, KMUTNB
+// Date: 2017-04-20
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __SI7021_H
+#define __SI7021_H
+
+#include <Wire.h>
+
+#define SI7021_MEASTEMP_NOHOLD_CMD     (0xF3)
+#define SI7021_MEASRH_NOHOLD_CMD       (0xF5)
+#define SI7021_RESET_CMD               (0xFE)
+#define SI7021_READ_USER_REG           (0xE7)
+
+#define SI7021_I2C_ADDR                (0x40)
+
+class SI7021 {
+  
+  public:
+    SI7021( uint8_t sda, uint8_t scl, uint8_t addr = SI7021_I2C_ADDR ) {
+        _addr = addr;
+        _sda  = sda;
+        _scl  = scl;
+        _temp  = 0.0f;
+        _humid = 0.0f;
+        _data_valid = false;
+    }
+
+    void begin() {
+        Wire.begin( _sda, _scl );
+        Wire.setClock( 400000 );
+    }
+
+    uint16_t readRaw( uint8_t cmd ) {
+        uint16_t value = 0;
+
+        _data_valid = false;
+        // Start I2C Transmission
+        Wire.beginTransmission( _addr );
+        Wire.write( cmd );
+        // Stop I2C transmission
+        Wire.endTransmission();
+        delay(25);
+
+        // Request 3 bytes of data
+        Wire.requestFrom( (int) _addr, (int) 3 ); // request 3 bytes
+
+        if ( Wire.available() == 3 ) {
+            _data[0] = Wire.read(); // (MSB)
+            _data[1] = Wire.read(); // (LSB)
+            _data[2] = Wire.read(); // (CRC8)
+            Wire.endTransmission();
+            if ( CRC8(_data, 2) == _data[2] ) {
+               _data_valid = true;
+               value = _data[0];
+               value = (value << 8) | _data[1];
+            }
+        } 
+        return value;
+    }
+
+    bool readMeasurement( ) {
+        uint16_t value;
+
+        value = readRaw( SI7021_MEASTEMP_NOHOLD_CMD );
+        if ( value != 0 && _data_valid ) {
+           _temp = value;
+           _temp *= 175.72f;
+           _temp /= 65536;
+           _temp -= 46.85f;
+        } else {
+            return false;
+        }
+
+        value = readRaw( SI7021_MEASRH_NOHOLD_CMD );
+        if ( value != 0 && _data_valid ) {
+           _humid  = value;
+           _humid *= 125;
+           _humid /= 65536;
+           _humid -= 6;
+        } else {
+           return false;
+        }
+        return true;
+    }
+
+    boolean isValid() { return _data_valid; }
+    float getTemperature() { return _temp; }
+    float getHumidity() { return _humid; }
+
+  protected:
+
+    uint8_t CRC8( const uint8_t *data, uint8_t len ) {
+       uint8_t crc = 0x00;
+       int i, j;
+       for ( i=0; i < len; ++i ) {
+            crc ^= data[i];
+            for ( j=8; j > 0; --j) {
+                if ( crc & 0x80 ) {
+                    crc = (crc << 1) ^ 0x131;
+                } else {
+                   crc = (crc << 1);
+               }
+            }
+       }
+       return crc;
+   }
+
+  private:
+     uint8_t _sda, _scl, _addr;
+     uint8_t _data[3];
+     float _temp, _humid;
+     boolean _data_valid;
+};
+
+#endif 
+////////////////////////////////////////////////////////////////////////////////
+
---
+++ esp8266_pcf8574a_lcd_si7021_demo/esp8266_pcf8574a_lcd_si7021_demo.ino
@@ -0,0 +1,108 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Author: RSP
+// IoT Engineering Education, KMUTNB
+// Date: 2017-04-20
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <ESP8266WiFi.h>
+
+#include <Wire.h> // use the Wire library
+#include "LiquidCrystal_I2C.h" // https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
+#include "SI7021.h" 
+
+#define I2C_SCL_PIN       (0)       //  GPIO-0 (D4)
+#define I2C_SDA_PIN       (2)       //  GPIO-2 (D4)
+#define I2C_LCD_ADDR          (0x3F)    // use 0x27 or 0x3F
+#define I2C_BH1750_ADDR       (0x23)
+
+//-------------------------------------------------------------------
+LiquidCrystal_I2C lcd( I2C_LCD_ADDR, 16, 2 ); // 16x2 LCD display, set I2C address
+SI7021  si7021( I2C_SDA_PIN, I2C_SCL_PIN, 0x40 );
+
+char sbuf[64];
+uint32_t ts;
+
+#define INTERVAL_MSEC  (2000)
+
+void i2c_scan() {
+  int count = 0;
+  Serial.println( "\n\nScanning I2C slave devices..." );
+  for( uint8_t addr=0x01; addr <= 0x7f; addr++ ) {
+     Wire.beginTransmission( addr ); 
+     if ( Wire.endTransmission() == 0 ) {
+       sprintf( sbuf, "I2C device found at 0x%02X.", addr );
+       Serial.println( sbuf );
+       count++;
+    }
+  }
+  if ( count > 0 ) {
+    sprintf( sbuf, "Found %d I2C device(s).\n\n", count );
+  } else {
+    sprintf( sbuf, "No I2C device found.\n\n" );
+  }
+  Serial.println( sbuf );
+}
+
+inline void i2c_write_byte( uint8_t i2c_addr, uint8_t data ) {
+  Wire.beginTransmission( i2c_addr );
+  Wire.write( data );
+  Wire.endTransmission();
+}
+
+int32_t temp, humid;
+
+//-------------------------------------------------------------------
+void setup() {
+  Serial.begin( 115200 );
+  Serial.println( "\n\n\n" );
+
+  WiFi.disconnect();
+  WiFi.mode( WIFI_OFF );
+
+  Wire.begin( I2C_SDA_PIN, I2C_SCL_PIN );
+  delay(100);
+  i2c_scan();
+  delay(100);
+
+  lcd.begin( I2C_SDA_PIN, I2C_SCL_PIN, 400000 );
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0,0); // set cursor at top-level position on the first row
+  lcd.print( "IoT Engineering" );
+  lcd.setCursor(0,1); // set cursor at the start position on the second row
+  lcd.print( "Education" );
+  delay(4000);
+  lcd.clear();
+  lcd.setCursor(0,0); // set cursor at top-level position on the first row  
+  lcd.print( "Si7021 Reading..." );
+  ts = millis();
+}
+
+void loop() {
+  if ( millis() - ts >= INTERVAL_MSEC ) {
+     ts = millis();
+     
+     if ( si7021.readMeasurement() ) {
+         temp = (int32_t)(10*si7021.getTemperature());
+         humid = (int32_t)(10*si7021.getHumidity());
+         sprintf( sbuf, "Temp : %d.%d degC, ", temp/10, temp%10 );
+         Serial.print( sbuf );
+         lcd.setCursor( 0 /*col*/, 0 /*row*/ ); 
+         lcd.print( sbuf );
+         sprintf( sbuf, "Humid: %d.%d %%RH", humid/10, humid%10 );
+         Serial.println( sbuf );
+         Serial.print( sbuf );
+         lcd.setCursor( 0 /*col*/, 1 /*row*/ ); 
+         lcd.print( sbuf );
+     } else {
+         Serial.println( "Sensor reading failed!!!\r\n" );
+         lcd.setCursor( 0 /*col*/, 0 /*row*/ ); 
+         lcd.print( "Sensor Reading" );
+         lcd.setCursor( 0 /*col*/, 1 /*row*/ ); 
+         lcd.print( "Status: FAILED " );
+     }
+  }
+  delay(1);
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+
